06e2f256d7b4d06629c95ebc7e00a6443f1f07c3
daviddorf2023 <daviddorf2023@u.northwestern.edu>
2023-01-28 21:26:26 -0600

    Added PPG data recordings using the QTPy microcontroller with QWIIC connection (with and without circular buffer).

--- src/logplot.py
+++ src/logplot.py
@@ -2,25 +2,19 @@ import matplotlib.pyplot as plt
 
 # open the putty log file
 # with open('putty.log') as f:
-with open('ppg_buffer.log') as f:
+with open('ppg_qt_buffer.log') as f:
     # read the file into a list
     lines = f.readlines()
 
 signalarray = []
 for line in lines:
-    # remove the newline character
     line = line.rstrip()
-    # split the line into a list of strings
     line = line.split()
-    # convert the list of strings into a list of floats
     line = float(line[0])
-    # add the line to the signal array
     signalarray.append(line)
-    # plot the line
     print(line)
     
 plt.xlabel('Time Frame (9600 Baud)')
-# EWMA is the Exponentially Weighted Moving Average
-plt.ylabel('PPG Signal Strength Squared (EWMA)')
+plt.ylabel('PPG Signal')
 plt.plot(signalarray)
 plt.show()
