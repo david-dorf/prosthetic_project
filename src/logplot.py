435f81d308a271c3be04536e5802842e71dbea77
daviddorf2023 <daviddorf2023@u.northwestern.edu>
2023-01-28 20:48:35 -0600

    Created a program using a circular buffer to eliminate signal drift and read PPG sensor data. Updated with new figures and Putty log file.

--- src/logplot.py
+++ src/logplot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 # open the putty log file
 # with open('putty.log') as f:
-with open('ppg.log') as f:
+with open('ppg_buffer.log') as f:
     # read the file into a list
     lines = f.readlines()
 
