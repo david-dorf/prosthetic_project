15a74fef364df857d87c2a08c93ecf8cef0b64aa
daviddorf2023 <daviddorf2023@u.northwestern.edu>
2023-02-02 16:57:13 -0600

    Moved .log files with logplot.py to a separate directory called "data" for organization

---
+++ data/logplot.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+# open the putty log file
+# with open('putty.log') as f:
+with open('ppg_qt_buffer_ewma.log') as f:
+    # read the file into a list
+    lines = f.readlines()
+
+signalarray = []
+for line in lines:
+    line = line.rstrip()
+    line = line.split()
+    line = float(line[0])
+    signalarray.append(line)
+    print(line)
+    
+plt.xlabel('Time Frame (9600 Baud)')
+plt.ylabel('PPG Signal')
+plt.plot(signalarray)
+plt.show()
